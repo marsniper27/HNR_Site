bad8c428a04c58b06e351eff1763f76a2123edb8
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2022-02-21 21:22:22 -0400

    complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,45 @@
             <a href="https://samurai.financial" target="_new">Samuari Financial</a>
         </p>
         <div class="cards">
-           
+            <div class="container">
+                <div class="card" style="min-width:50%">
+                    <div class="grid">
+                        <div class="label">
+                            <h2>Current HNR Balance: {{Balance}}</h2>
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <div class="label">
+                            <h2>Last Claim: {{claimDate}}</h2>
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <button @click="cashOut()">Claim reward</button>
+                    </div>
+                </div>
+            </div>           
+        </div>
+        <div class="cards">
+            <div class="container">
+                <div class="card" style="min-width:50%">
+                    <div class="grid">
+                        <div class="label">
+                            <h2>Create Musha Node</h2>
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <div class="label">
+                            <h2>Node Name: </h2>
+                        </div>
+                        <div class="cont  large-text">
+                            <input v-model="nodeName" placeholder="New Node Name" />
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <button @click="createNode()">Create Musha Node</button>
+                    </div>
+                </div>
+            </div>           
         </div>
     </div>
     <div v-if="messages" class="messages">
@@ -37,13 +75,15 @@ export default {
     components: {},
     data() {
         return {
-            logoMain:logoMain,
             account: "Not Connected",
             web3:null,
             messages:false,
             connected:false,
             starAdded:false,
             web3: null,
+            Balance:null,
+            claimDate:null,
+            nodeName:null
         }
     },
     watch: {
@@ -58,8 +98,7 @@ export default {
             console.log('MetaMask is installed!');
             if(this.$route.params.web3 == null || this.$route.params.account == null){
                 console.log("account not set");
-                await this.matics();
-                initMasterchef();
+                await this.matics();;
             }
             else{
                 console.log("account already set");
@@ -68,6 +107,7 @@ export default {
                 this.web3 = this.$route.params.web3;
                 var chainId = new this.web3.eth.getChainId();
                 if(chainId != 0xFA){Functions.setChain()};
+                this.getBalance();
             }
         }
         else{
@@ -96,8 +136,10 @@ export default {
                         if(accounts.length > 0){
                             this.connected = true;
                             this.account = accounts[0];
+                            console.log(this.account)
                             this.messages = " Account: " + this.account;
                             setTimeout(d=>{this.messages = false},1000);
+                            this.getBalance();
                         }
                         else{
                             this.messages = "No account Connected"
@@ -108,6 +150,15 @@ export default {
                     })
                 }
             })
+        },
+        async getBalance(){
+            this.Balance = await Functions.getBalance(this.web3,this.account);
+        },
+        async cashOut(){
+            await Functions.cashoutAll(this.web3,this.account);
+        },
+        async createNode(){
+            await Functions.createNode(this.web3,this.nodeName,this.account)
         },
         async disconnect(){
             this.account = "Not Connected";
